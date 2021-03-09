4d3e45ff7ce2522da7f659c14e851830abd3f9f0
datquocngo <shaco14091999@gmail.com>
2021-03-09 11:46:21 +0700

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {MainComponent} from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
